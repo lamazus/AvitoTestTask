d90bdfbada0adc327f36c8b00ccca5df53214002
lamazus <flooop08@gmail.com>
2023-08-31 13:36:18 +0300

    decrease timeout

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 
 class BasePage():
-    def __init__(self, browser:webdriver.Chrome, url:str, timeout=3):
+    def __init__(self, browser:webdriver.Chrome, url:str, timeout=1):
         self.browser = browser
         self.url = url
         self.browser.implicitly_wait(timeout)
